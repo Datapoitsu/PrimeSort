44268b2658ea3d37ccad48801196ae244366df34
Thaumatized <veeti.junkkala@gmail.com>
2023-02-18 14:43:24 +0200

    Sort works now

---
+++ PrimeSort.py
@@ -0,0 +1,71 @@
+import random
+import sys
+import PrimeNumbers
+
+def Sorted(arr):
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
+
+def Primesort(arr):
+    PrimeIndex = 0
+    while not Sorted(arr):
+        changeIndex = PrimeNumbers.CalculatePrimesIndex(PrimeIndex) % len(arr)
+        holder = arr[changeIndex]
+        for i in range(changeIndex, 0, -1):
+            arr[i] = arr[i-1]
+        arr[0] = holder
+        PrimeIndex+=1
+
+        
+def RandArray(length = 10, maxvalue=255):
+    arr = []
+    for i in range(length):
+        arr.append(random.randint(0, maxvalue))
+    return arr
+    
+if __name__ == '__main__':
+    length = 5
+    maxvalue = 255
+    arr = []
+
+    #Args
+    i = 1
+    while i < len(sys.argv):
+        '''Waiting for python3.10
+            case "-l":
+                length = int(sys.argv[i+1])
+                i+=1
+            case "-m":
+                maxvalue = int(sys.argv[i+1])
+                i+=1
+            case "-a":
+                while i+1 < len(sys.argv) and sys.argv[i+1].isnumeric():
+                    arr.append(int(sys.argv[i+1]))
+                    i+=1
+        i+=1
+        '''
+        if sys.argv[i] == "-l":
+            length = int(sys.argv[i+1])
+            i+=1
+        if sys.argv[i] == "-m":
+            maxvalue = int(sys.argv[i+1])
+            i+=1
+        if sys.argv[i] == "-a":
+            while i+1 < len(sys.argv) and sys.argv[i+1].isnumeric():
+                arr.append(int(sys.argv[i+1]))
+                i+=1
+        i+=1
+        
+    
+    #if empty generate random array and print it
+    if arr == []:
+        arr = RandArray(length, maxvalue)
+        for number in arr:
+            print(str(number) + ", ", end="")
+        print()
+    Primesort(arr)
+    for number in arr:
+        print(str(number) + ", ", end="")
+
---
+++ README.md
@@ -0,0 +1,8 @@
+Primesort is a successor to bogosort. It has been impruved by using primes to shuffle the array, instead of actual random numbers. This makes it consistent with the same array, and only increases the compute time by a ton!
+
+Starting array is printed only if -a is not used.  
+
+Command line arguments:
+	-l length, how long of a list is used. default 10.  
+	-m max, biggest number allowed in the list. default 255.  
+	-a values..., values used to specify an array to sort. overrides -c and -v.
