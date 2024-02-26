adac410470719986644211fc7c3e2a2da973148d
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-02-26 18:35:23 +0200

    More optimization on primenumbers

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -12,34 +12,33 @@ def CalculatePrimesIndex(n): #Returns the n:th prime
     if n <= len(primes) - 1:
         return primes[n]
     
-    num = primes[-1]#Starts from the two above last known prime
+    num = primes[-1] #Starts from last known prime
     while True:
-        num += 2
-        while int(str(num)[-1]) not in [1,3,7,9]: #Increases the number, till it is possible to be a prime
+        num += 2 #Because primes are odd (except number 2), there is no point to check any odd numbers
+        if str(num)[-1] == '5': #numbers ending with digit 5 aren't primes (except 5 it)
             num += 2
 
         #Prime can't be divisible with numbers higher that it's sqrt
-        sqrt = math.sqrt(num)
+        sqrt = int(math.sqrt(num))
                 
         isPrime = True    
         for k in range(1,len(primes)): #Doesn't have to test number 2, because num can't be an even number
-            if primes[k] > sqrt:
+            if primes[k] > sqrt: #Limiter, so code doesn't test unnecessary numbers.
                 break
             if num % primes[k] == 0: #If division goes perfectly onto number,
                 isPrime = False #Then it isn't a prime
                 break
+        
         if isPrime == True: #It was a prime!
             primes.append(num)
-            if n <= len(primes) - 1: #If enough primes have been calculated,
+            if n == len(primes) - 1: #If enough primes have been calculated,
                 return primes[n] #then will break and return the correct prime
-            
-    
-    
+
 if __name__ == '__main__':
     # -- Testing the performance -- #
     import time
     Alku = time.time()
-    dist = 4
+    dist = 5
     for i in range(1,dist):
         print(CalculatePrimesIndex(10 ** i))
     for i in range(dist,0,-1):
